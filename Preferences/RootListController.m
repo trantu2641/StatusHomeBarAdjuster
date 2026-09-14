75078cbef593bb8e6757439e448ad58a47230686
trantu2641 <trantu2641@gmail.com>
2026-09-14 15:47:39 +0700

    Update RootListController.m

--- Preferences/RootListController.m
+++ Preferences/RootListController.m
@@ -1,7 +1,4 @@
 #import "RootListController.h"
-#import <Preferences/Preferences.h>
-#import <UIKit/UIKit.h>
-#import <notify.h>
 
 @implementation RootListController
 
@@ -14,34 +11,6 @@
     }
 
     return _specifiers;
-}
-
-- (void)applySettings
-{
-    NSUserDefaults *defaults =
-        [[NSUserDefaults alloc]
-            initWithSuiteName:@"com.congtu.statushomebaradjuster"];
-
-    NSInteger status =
-        [defaults integerForKey:@"StatusBarOffset"];
-
-    NSInteger home =
-        [defaults integerForKey:@"HomeBarOffset"];
-
-    status = MAX(-120, MIN(120, status));
-    home = MAX(-120, MIN(120, home));
-
-    [defaults setInteger:status
-                  forKey:@"StatusBarOffset"];
-
-    [defaults setInteger:home
-                  forKey:@"HomeBarOffset"];
-
-    [defaults synchronize];
-
-    notify_post(
-        "com.congtu.statushomebaradjuster.settingsChanged"
-    );
 }
 
 @end
