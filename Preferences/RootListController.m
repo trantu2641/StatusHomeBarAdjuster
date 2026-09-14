e2fa4f93ef31052b79c96b051fe77e5798c34fe9
trantu2641 <trantu2641@gmail.com>
2026-09-14 17:06:47 +0700

    Update RootListController.m

--- Preferences/RootListController.m
+++ Preferences/RootListController.m
@@ -22,26 +22,18 @@
         [[NSUserDefaults alloc]
             initWithSuiteName:@"com.congtu.statushomebaradjuster"];
 
-    NSInteger status =
-        [defaults integerForKey:@"StatusBarOffset"];
-
-    NSInteger home =
-        [defaults integerForKey:@"HomeBarOffset"];
+    NSInteger status = [defaults integerForKey:@"StatusBarOffset"];
+    NSInteger home = [defaults integerForKey:@"HomeBarOffset"];
 
     status = MAX(-120, MIN(120, status));
     home = MAX(-120, MIN(120, home));
 
-    [defaults setInteger:status
-                  forKey:@"StatusBarOffset"];
-
-    [defaults setInteger:home
-                  forKey:@"HomeBarOffset"];
+    [defaults setInteger:status forKey:@"StatusBarOffset"];
+    [defaults setInteger:home forKey:@"HomeBarOffset"];
 
     [defaults synchronize];
 
-    notify_post(
-        "com.congtu.statushomebaradjuster.settingsChanged"
-    );
+    notify_post("com.congtu.statushomebaradjuster.settingsChanged");
 }
 
 @end
